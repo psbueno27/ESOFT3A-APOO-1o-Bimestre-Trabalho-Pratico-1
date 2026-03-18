e0f78aebc34d556eadcc6ddb9c802f3f32abfcaf
Juan Siqueira <juanpablopss.12@gmail.com>
2026-03-18 16:11:42 -0300

    Update and rename Exercicio8.java to exercicio08.java

--- exercicio08.java
+++ exercicio08.java
@@ -13,7 +13,7 @@
 
 import java.util.Scanner;
 
-public class Exercicio8 {
+public class exercicio08 {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
