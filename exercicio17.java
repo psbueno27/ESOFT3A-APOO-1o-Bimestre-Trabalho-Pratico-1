a7780fdc4cb5e7112c90940cda7c3874be290561
psbueno27 <buenopedro07@gmail.com>
2026-03-18 17:52:27 -0300

    Update exercicio17.java

--- exercicio17.java
+++ exercicio17.java
@@ -7,7 +7,7 @@
  25004652-2 - Alef Gustavo Antunes Luciano
  25181903-2 - Juan Pablo da Silva Siqueira
  25181992-2 - Pedro de Souza Bueno
- Data: 05 de Março de 2026
+ Data: 18 de Março de 2026
  Descritivo: Desenvolva um jogo onde o computador sorteie um número entre 1 e 100, e o usuário tente adivinhá-lo.
  O programa deve dar dicas ("maior" ou "menor") até o acerto.
  *******************************************************************************/
