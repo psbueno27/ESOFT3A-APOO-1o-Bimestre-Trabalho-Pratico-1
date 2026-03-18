88e5d74774cee1621df32bfe0d4fd2ab375fda27
psbueno27 <buenopedro07@gmail.com>
2026-03-18 17:51:29 -0300

    Revise header comments and update date
    Updated header comments and date in exercicio14.java.

--- exercicio14.java
+++ exercicio14.java
@@ -7,7 +7,7 @@
  25004652-2 - Alef Gustavo Antunes Luciano
  25181903-2 - Juan Pablo da Silva Siqueira
  25181992-2 - Pedro de Souza Bueno
- Data: 05 de Março de 2026
+ Data: 18 de Março de 2026
  Descritivo: Escreva um programa que solicite um número inteiro e exiba sua tabuada de 1 a 10.
  *******************************************************************************/
 
