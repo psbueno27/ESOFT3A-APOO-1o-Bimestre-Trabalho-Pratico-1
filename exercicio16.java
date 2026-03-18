6f7f2f10a97465f15f5e435998b9a4fac9fcd769
psbueno27 <buenopedro07@gmail.com>
2026-03-18 17:52:11 -0300

    Update exercicio16.java

--- exercicio16.java
+++ exercicio16.java
@@ -7,7 +7,7 @@
  25004652-2 - Alef Gustavo Antunes Luciano
  25181903-2 - Juan Pablo da Silva Siqueira
  25181992-2 - Pedro de Souza Bueno
- Data: 05 de Março de 2026
+ Data: 18 de Março de 2026
  Descritivo: Escreva um programa que valide uma senha de acordo com os seguintes critérios:
  -Pelo menos 8 caracteres
  -Pelo menos uma letra maiúscula
